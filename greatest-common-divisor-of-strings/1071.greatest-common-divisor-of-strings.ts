885175377ba7ed68bfc1ef8ffb393abc348ea9f3
Bruno Enrique <brunobaronenrique@gmail.com>
2023-07-14 17:01:42 -0300

    add greatest commom divisor of string challenge

---
+++ greatest-common-divisor-of-strings/1071.greatest-common-divisor-of-strings.ts
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=1071 lang=typescript
+ *
+ * [1071] Greatest Common Divisor of Strings
+ *
+ * https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
+ *
+ * algorithms
+ * Easy (55.46%)
+ * Likes:    3741
+ * Dislikes: 648
+ * Total Accepted:    197.3K
+ * Total Submissions: 362.1K
+ * Testcase Example:  '"ABCABC"\n"ABC"'
+ *
+ * For two strings s and t, we say "t divides s" if and only if s = t + ... + t
+ * (i.e., t is concatenated with itself one or more times).
+ *
+ * Given two strings str1 and str2, return the largest string x such that x
+ * divides both str1 and str2.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: str1 = "ABCABC", str2 = "ABC"
+ * Output: "ABC"
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: str1 = "ABABAB", str2 = "ABAB"
+ * Output: "AB"
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: str1 = "LEET", str2 = "CODE"
+ * Output: ""
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= str1.length, str2.length <= 1000
+ * str1 and str2 consist of English uppercase letters.
+ *
+ *
+ */
+
+// @lc code=start
+function gcdOfStrings(str1: string, str2: string): string {
+	if (str1 + str2 !== str2 + str1) return '';
+	return str1.substring(0, gcd(str1.length, str2.length));
+}
+
+let gcd = function (a, b) {
+	if (!b) {
+		return a;
+	}
+
+	return gcd(b, a % b);
+};
+// @lc code=end
